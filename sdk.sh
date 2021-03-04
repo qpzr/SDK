2989d90486c88d39e89cabfa80aca454b6928826
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-04 08:43:09 +0800

    Update sdk.sh

--- sdk.sh
+++ sdk.sh
@@ -1,6 +1,6 @@
 wget -cO sdk.tar.xz https://openwrt.cc/releases/targets/ipq40xx/generic/openwrt-sdk-ipq40xx-generic_gcc-8.4.0_musl_eabi.Linux-x86_64.tar.xz
 mkdir -p sdk
-tar -xJf *.tar.xz -C sdk
+tar -xJf sdk.tar.xz -C sdk
 cp -rf sdk/*/staging_dir/* ./staging_dir/
 rm -rf sdk.tar.xz sdk
 sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile
