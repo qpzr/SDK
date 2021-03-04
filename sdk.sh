bc9ed3ca3c41acd7aeedcfd3e3d9685cc16df50b
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-04 08:54:53 +0800

    Update sdk.sh

--- sdk.sh
+++ sdk.sh
@@ -1,8 +1,13 @@
-wget -cO sdk.tar.xz https://openwrt.cc/releases/targets/ipq40xx/generic/openwrt-sdk-ipq40xx-generic_gcc-8.4.0_musl_eabi.Linux-x86_64.tar.xz
-mkdir -p sdk
-tar -xJf sdk.tar.xz -C sdk
-cp -rf sdk/*/staging_dir/* ./staging_dir/
+#!/bin/sh
+
+DIR="sdk"
+DL_NAME="openwrt-sdk-ipq40xx-generic_gcc-8.4.0_musl_eabi.Linux-x86_64.tar.xz"
+DL_URL="https://openwrt.cc/releases/targets/ipq40xx/generic/$DL_NAME"
+curl -O -L $DL_URL && \
+mkdir -p $DIR && \
+tar -xvf $DL_NAME -C $DIR && \
+cp -rf sdk/*/staging_dir/* ./staging_dir/ && \
 rm -rf sdk.tar.xz sdk
-sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile
-ln -sf /usr/bin/python staging_dir/host/bin/python
+sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile && \
+ln -sf /usr/bin/python staging_dir/host/bin/python && \
 ln -sf /usr/bin/python staging_dir/host/bin/python3
