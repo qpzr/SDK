581a6c81312d4f1bb32b07c7ef49679ab494301b
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-04 13:22:52 +0800

    Add files via upload

--- sdk.sh
+++ sdk.sh
@@ -5,4 +5,4 @@ cp -rf sdk/*/staging_dir/* ./staging_dir/
 rm -rf sdk.tar.xz sdk
 sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile
 ln -sf /usr/bin/python staging_dir/host/bin/python
-ln -sf /usr/bin/python staging_dir/host/bin/python3 
+ln -sf /usr/bin/python staging_dir/host/bin/python3
