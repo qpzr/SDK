f67ab83c32ddd69d0c5a65946a719deb698f0f27
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-04 10:55:36 +0800

    Add files via upload

--- sdk.sh
+++ sdk.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-DIR="sdk"
-DL_NAME="openwrt-sdk-ipq40xx-generic_gcc-8.4.0_musl_eabi.Linux-x86_64.tar.xz"
-DL_URL="https://openwrt.cc/releases/targets/ipq40xx/generic/$DL_NAME"
-curl -O -L $DL_URL && \
-mkdir -p $DIR && \
-tar -xvf $DL_NAME -C $DIR && \
-cp -rf sdk/*/staging_dir/* ./staging_dir/ && \
+curl -O -L https://github.com/qpzr/SDK/releases/download/v5.4/sdk.tar.xz
+mkdir -p sdk
+tar -xJf sdk.tar.xz -C sdk
+cp -rf sdk/*/staging_dir/* ./staging_dir/
 rm -rf sdk.tar.xz sdk
-sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile && \
-ln -sf /usr/bin/python staging_dir/host/bin/python && \
+sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile
+ln -sf /usr/bin/python staging_dir/host/bin/python
 ln -sf /usr/bin/python staging_dir/host/bin/python3
